c541ab9e9148245dd0c48e8b6a73762157544bf3
skillup <test@test.com>
2019-11-16 12:52:23 +0200

    v4444

--- server/public/login.js
+++ server/public/login.js
@@ -3,6 +3,7 @@ var userName = document.querySelector('#username');
 var userEmail = document.querySelector('#useremail');
 var userPass = document.querySelector('#userpass');
 var userSubscribe = document.querySelector('#usersubscribe');
+
 var sendBtn = document.querySelector('#sendbtn');
 
 console.log(sendBtn);
@@ -10,6 +11,12 @@ console.log(sendBtn);
 sendBtn.onclick = send;
 
 function send () {
+
+    if(!(/^[а-яa-z0-9\-_\.]{2,25}$/i.test(userName.value))) {
+        alert('not valid name [а-яa-z0-9\-_\.]{2,25}')
+        return false;
+    }
+
 
     var data = {
         name: userName.value,
