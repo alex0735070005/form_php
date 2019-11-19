d7ab0896ed442d5b5aaaf86045b16c003185d382
skillup <test@test.com>
2019-11-19 21:42:36 +0200

    v333

--- server/public/login.js
+++ server/public/login.js
@@ -1,21 +1,44 @@
 'use strict'
 var userName = document.querySelector('#username');
+var userNameError = document.querySelector('#username + .auth__error');
+
+
 var userEmail = document.querySelector('#useremail');
+var userEmailError = document.querySelector('#useremail + .auth__error');
+
+var userPhone = document.querySelector('#userphone');
+var userPhoneError = document.querySelector('#userphone + .auth__error');
+
 var userPass = document.querySelector('#userpass');
 var userSubscribe = document.querySelector('#usersubscribe');
 
 var sendBtn = document.querySelector('#sendbtn');
 
-console.log(sendBtn);
-
 sendBtn.onclick = send;
 
-function send () {
+function send() {
 
-    if(!(/^[а-яa-z0-9\-_\.]{2,25}$/i.test(userName.value))) {
-        alert('not valid name [а-яa-z0-9\-_\.]{2,25}')
-        return false;
+    var isError = false;
+
+    if (!(/^[а-яa-z0-9\-_\.]{2,25}$/i.test(userName.value))) {
+        userNameError.classList.remove('auth__error_hide');
+        userNameError.classList.add('auth__error_show');
+        isError = true;
     }
+
+    if (!(/^[0-9a-z.\-_]{1,15}@[0-9a-z.\-_]{1,14}\.[a-z]{1,10}$/i.test(userEmail.value))) {
+        userEmailError.classList.remove('auth__error_hide');
+        userEmailError.classList.add('auth__error_show');
+        isError = true;
+    }
+
+    if (!(/^(\+380|380|80|0){1,4}[0-9]{9}$/.test(userPhone.value))) {
+        userPhoneError.classList.remove('auth__error_hide');
+        userPhoneError.classList.add('auth__error_show');
+        isError = true;
+    }
+
+    if(isError) return null;
 
 
     var data = {
@@ -26,7 +49,20 @@ function send () {
     }
 
     fetch('/login', {
-        method:'POST',
+        method: 'POST',
         body: JSON.stringify(data)
     });
 }
+
+
+function setClearHandler() {
+    var elements = document.querySelectorAll('.auth__text');
+
+    elements.forEach(function (element) {
+        element.onclick = function () {            
+            this.nextElementSibling.classList.remove('auth__error_show');
+            this.nextElementSibling.classList.add('auth__error_hide');
+        }
+    });
+}
+setClearHandler();
--- server/views/login.php
+++ server/views/login.php
@@ -4,15 +4,26 @@
         <div class="auth__row">
             <label for="username">User name</label>
             <input class="auth__text" type="text" id="username">
-            <i class="auth__error">Don`t contain (_, -, {}, $, 0-9 length > 5 chars)</i>
+            <i class="auth__error auth__error_hide">Don`t contain (_, -, {}, $, 0-9 length > 5 chars)</i>
         </div>
+
+        
         <div class="auth__row">
             <label for="useremail">User email</label>
             <input class="auth__text" type="text" id="useremail">
+            <i class="auth__error auth__error_hide">Not valid email (example@gmail.com)</i>
         </div>
+
+        <div class="auth__row">
+            <label for="userphone">User phone</label>
+            <input class="auth__text" type="text" id="userphone">
+            <i class="auth__error auth__error_hide">Not valid phone (+380735070005)</i>
+        </div>
+
         <div class="auth__row">
             <label for="userpass">User password</label>
             <input class="auth__text" type="password" id="userpass">
+            <i class="auth__error auth__error_hide">Must contain (a-z, A-Z, 0-9, length > 7 chars)</i>
         </div>
 
         <label class="auth__check">
