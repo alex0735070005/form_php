b1b49dc0cc81917d71fca278095124b872605950
skillup <test@test.com>
2019-11-23 12:41:21 +0200

    v44224352422

--- server/public/login.js
+++ server/public/login.js
@@ -1,51 +1,59 @@
 'use strict'
-var userName = document.querySelector('#username');
-var userNameError = document.querySelector('#username + .auth__error');
 
+var formManager = {
+    name: document.querySelector('#username'),
+    nameError: document.querySelector('#username + .auth__error'),
+    email: document.querySelector('#useremail'),
+    emailError: document.querySelector('#useremail + .auth__error'),
+    phone: document.querySelector('#userphone'),
+    phoneError: document.querySelector('#userphone + .auth__error'),
+    password: document.querySelector('#userpass'),
+    passwordError: document.querySelector('#userpass + .auth__error'),
+    subscribe: document.querySelector('#usersubscribe'),
+    sendBtn: document.querySelector('#sendbtn'),    
+};
 
-var userEmail = document.querySelector('#useremail');
-var userEmailError = document.querySelector('#useremail + .auth__error');
+formManager.valid = function () {
+    var isNotError = true;
 
-var userPhone = document.querySelector('#userphone');
-var userPhoneError = document.querySelector('#userphone + .auth__error');
-
-var userPass = document.querySelector('#userpass');
-var userSubscribe = document.querySelector('#usersubscribe');
-
-var sendBtn = document.querySelector('#sendbtn');
-
-sendBtn.onclick = send;
-
-function send() {
-
-    var isError = false;
-
-    if (!(/^[а-яa-z0-9\-_\.]{2,25}$/i.test(userName.value))) {
-        userNameError.classList.remove('auth__error_hide');
-        userNameError.classList.add('auth__error_show');
-        isError = true;
+    if (!(/^[а-яa-z0-9\-_\.]{2,25}$/i.test(this.name.value))) {
+        this.nameError.classList.remove('auth__error_hide');
+        this.nameError.classList.add('auth__error_show');
+        isNotError = false;
     }
 
-    if (!(/^[0-9a-z.\-_]{1,15}@[0-9a-z.\-_]{1,14}\.[a-z]{1,10}$/i.test(userEmail.value))) {
-        userEmailError.classList.remove('auth__error_hide');
-        userEmailError.classList.add('auth__error_show');
-        isError = true;
+    if (!(/^[0-9a-z.\-_]{1,15}@[0-9a-z.\-_]{1,14}\.[a-z]{1,10}$/i.test(this.email.value))) {
+        this.emailError.classList.remove('auth__error_hide');
+        this.emailError.classList.add('auth__error_show');
+        isNotError = false;
     }
 
-    if (!(/^(\+380|380|80|0){1,4}[0-9]{9}$/.test(userPhone.value))) {
-        userPhoneError.classList.remove('auth__error_hide');
-        userPhoneError.classList.add('auth__error_show');
-        isError = true;
+    if (!(/^(\+380|380|80|0){1,4}[0-9]{9}$/.test(this.phone.value))) {
+        this.phoneError.classList.remove('auth__error_hide');
+        this.phoneError.classList.add('auth__error_show');
+        isNotError = false;
     }
 
-    if(isError) return null;
+    if (!(/^[а-яa-z0-9\-_\.]{2,25}$/i.test(this.password.value))) {
+        this.passwordError.classList.remove('auth__error_hide');
+        this.passwordError.classList.add('auth__error_show');
+        isNotError = false;
+    }
 
+    return isNotError;
+}
+
+formManager.send = function () {
+
+    console.log(this)
+
+    if (this.valid() === false) return null;
 
     var data = {
-        name: userName.value,
-        email: userEmail.value,
-        password: userPass.value,
-        subscribe: userSubscribe.value,
+        name: this.name.value,
+        email: this.email.value,
+        password: this.password.value,
+        subscribe: this.subscribe.checked,
     }
 
     fetch('/login', {
@@ -55,14 +63,20 @@ function send() {
 }
 
 
-function setClearHandler() {
+formManager.setClearHandler = function () {
     var elements = document.querySelectorAll('.auth__text');
 
     elements.forEach(function (element) {
-        element.onclick = function () {            
+        element.onclick = function () {
             this.nextElementSibling.classList.remove('auth__error_show');
             this.nextElementSibling.classList.add('auth__error_hide');
         }
     });
 }
-setClearHandler();
+
+formManager.init = function() {
+    this.sendBtn.onclick = this.send.bind(this);
+    this.setClearHandler();
+}
+
+formManager.init();
